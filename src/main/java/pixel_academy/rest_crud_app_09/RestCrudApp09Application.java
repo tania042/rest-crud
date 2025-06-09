650c9bfe6a24eb68269a7e929f85bcc5973c842f
Tatiana Ceban <tatiana.ceban.19@bk.ru>
2025-06-09 18:14:27 +0300

    Initial commit

---
+++ src/main/java/pixel_academy/rest_crud_app_09/RestCrudApp09Application.java
@@ -0,0 +1,13 @@
+package pixel_academy.rest_crud_app_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudApp09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCrudApp09Application.class, args);
+	}
+
+}
---
+++ src/test/java/pixel_academy/rest_crud_app_09/RestCrudApp09ApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel_academy.rest_crud_app_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudApp09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
